cfe1ec218351631ce3b1576acf24ad3f9f173c8e
Kittel-Watne <142875697+Kittel-Watne@users.noreply.github.com>
2024-10-29 11:41:51 +0100

    Added GraphNode
    Made minor changes to buildSlot
    
    Added graphnode in order to separate those functions form BuildingSlot, This will avoid circular dependency, whilst still allowing Enemy to be able to interact with BuildingSlot, thinking it is a GraphNode

---
+++ Source/Programming3TD/Private/GraphNode.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GraphNode.h"
+
+// Sets default values
+AGraphNode::AGraphNode()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGraphNode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGraphNode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+double AGraphNode::GetValue(AGraphNode* endNode)
+{
+	return ThreatLevel + FVector::Dist(this->GetActorLocation(), endNode->GetActorLocation());;
+}
+
+
+bool AGraphNode::isAdjacent(AGraphNode* otherBuildingSlot)
+{
+	if (AdjacentMap.Contains(otherBuildingSlot))
+	{
+		return true;
+	}
+	return false;
+}
+
+void AGraphNode::AddAdjacent(AGraphNode* otherBuildingSlot)
+{
+	if (isAdjacent(otherBuildingSlot))
+	{
+		return;
+	}
+
+	AdjacentMap.Add(otherBuildingSlot, otherBuildingSlot);
+	ArrayOfAdjacencyMap.Add(otherBuildingSlot);
+
+	otherBuildingSlot->AddAdjacent(this); //this line is after to prevent an infinite loop
+}
+
+TArray<AGraphNode*> AGraphNode::GetAdjacent()
+{
+	return ArrayOfAdjacencyMap;
+}
