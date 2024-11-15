aa54f4b6300bb24fd6b652f35c733bc0a14432fc
pauliepus <142883643+pauliepus@users.noreply.github.com>
2024-11-15 12:30:14 +0100

    Added ConcreteWaveManager

---
+++ Source/Programming3TD/Public/ConcreteWaveManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbstractWaveManager.h"
+#include "ConcreteWaveManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMING3TD_API AConcreteWaveManager : public AAbstractWaveManager
+{
+	GENERATED_BODY()
+	
+};
