9bee8a30077940c39192004badc46df98916caf7
pauliepus <142883643+pauliepus@users.noreply.github.com>
2024-11-25 19:23:02 +0100

    RegenEnemy exists, no contstructor
    +BeginPlay()
    +EndPlay()

---
+++ Source/Programming3TD/Private/RegenEnemy.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RegenEnemy.h"
+
+void ARegenEnemy::BeginPlay()
+{
+}
+
+void ARegenEnemy::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+}
---
+++ Source/Programming3TD/Public/RegenEnemy.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbstractEnemy.h"
+#include "RegenEnemy.generated.h"
+
+/**
+ * @Author Paulius
+ */
+
+UCLASS()
+class PROGRAMMING3TD_API ARegenEnemy : public AAbstractEnemy
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+
+};
